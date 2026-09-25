b24993718b6ebc7c6bed2413c8dc3a59c7c86f41
Rizky Reranza <rranzabussines@gmail.com>
2026-09-25 17:02:15 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,5 +7,5 @@
 
 window.ContentSenseConfig = {
   SUPABASE_FUNCTION_URL: "https://wzokcxnnalvrrvgclwfb.supabase.co/functions/v1/analyze-text",
-  SUPABASE_ANON_KEY: "PASTE_ANON_PUBLIC_KEY_DI_SINI",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6b2tjeG5uYWx2cnJ2Z2Nsd2ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNjg4MzgsImV4cCI6MjA5Mjk0NDgzOH0.Az0j3K-SRxzXLaCAeggI6O-KKGxYfCRRW08WzPqAwTo",
 };
